4a46bc4afbfd7d6173d0adb44351079229c0e403
Jospine <nzi19001@byui.edu>
2024-07-13 21:28:19 +0300

    try env

--- app/confiq.py
+++ app/confiq.py
@@ -1,4 +1,4 @@
-DATABASE_URL='postgresql://josephine:root@localhost:5432/inventory_db1'
-APP_ENV='production'
+# DATABASE_URL='postgresql://josephine:root@localhost:5432/inventory_db1'
+# APP_ENV='production'
 DATABASE_URL='postgresql://inventory_api_8kjl_user:vltJzHktDbKhqFFyefC7mR1B9nIzutni@dpg-cq9851qju9rs73b1msc0-a.frankfurt-postgres.render.com/inventory_api_8kjl'
 APP_ENV='production'
