beaafdb3bc4cefda404729abb0a040347059e40e
Jospine <nzi19001@byui.edu>
2024-07-13 21:21:04 +0300

    try

--- app/confiq.py
+++ app/confiq.py
@@ -1,6 +1,4 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://josephine:root@localhost:5432/inventory_db1')
-    ENVIRONMENT = os.getenv('APP_ENV', 'development')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+DATABASE_URL='postgresql://josephine:root@localhost:5432/inventory_db1'
+APP_ENV='production'
+DATABASE_URL='postgresql://inventory_api_8kjl_user:vltJzHktDbKhqFFyefC7mR1B9nIzutni@dpg-cq9851qju9rs73b1msc0-a.frankfurt-postgres.render.com/inventory_api_8kjl'
+APP_ENV='production'
